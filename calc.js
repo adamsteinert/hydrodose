1816c778e1678c84f60e38c473b8de3ab6b6827c
Adam Steinert <alphajulietsierra@gmail.com>
2017-06-02 00:18:32 -0500

    Simple dosing loop working.

--- calc.js
+++ calc.js
@@ -27,41 +27,50 @@ let intervalCalc = function(targetVolume_ml, rate_ml_min, totalTimespan_min) {
 
 
 // uses interval and startDate to calculate the next run time.
-// atDate is for debugging and should be removed in production
-// Depends on startDate being equal to the current date
-let calculateNextRunTime = function(startDate, interval, atDate) {
-    let now = atDate || new moment();
+// relative to forDate (or NOW if omitted)
+//
+// Depends on startDate being equal to the current date!!
+// a return value of undefined indicates the current time is past the full run interval. 
+// The date part of startDate should be incremented to today's date to get the next run time.
+let calculateNextRunTime = function(startDate, interval, forDate) {
+    let now = forDate || new moment();
     let segmentNumber = 0;
     let currentMs = now - startDate;
     let currentRunTime = undefined;
 
     while(segmentNumber <= interval.divisor) {
 
-        var goTime = startDate.valueOf() + ((segmentNumber * interval.segmentTime_min) * 60 * 1000)
-        if(goTime >= now)
+        //startDate.clone().add(segmentNumber * interval.segmentTime_min, 'm'); //
+        var goTime = startDate.clone().add(segmentNumber * interval.segmentTime_min, 'm'); //startDate.valueOf() + ((segmentNumber * interval.segmentTime_min) * 60 * 1000)
+        if(goTime > now)
             return {
-                "thisRun" : currentRunTime,
-                "nextRun" : new moment(goTime),
-                "segment" : segmentNumber,
-                "now" : now
+                "now" : now,
+                "thisRun" : (currentRunTime || goTime), //if now is before startDate thisRun and nextRun are equivalent, thisStop will accurately reflect the initial segment stop.
+                "thisStop" : (currentRunTime || goTime).clone().add(interval.upTime_min, 'm'),
+                "nextRun" : goTime,
+                "segment" : segmentNumber
             };
         
-        currentRunTime = new moment(goTime); // track the most recent run time to be set as 'thisRun' on the next go around if the time checks.
+        currentRunTime = goTime; // track the most recent run time to be set as 'thisRun' on the next go around if the time checks.
         segmentNumber++;
     }
 
-    return undefined;
+
+    return undefined; 
 }
 
-let startDate = new moment("2017-05-01 08:00");
-let atDate    = new moment("2017-05-01 08:01");
-//let atDate    = new Date("2017-05-01 08:00");
 
+let startDate = new moment("2017-05-01 08:00");
+let atDate    = new moment("2017-06-01 07:00");
+//let atDate    = new Date("2017-05-01 08:00");
 
 let interval = intervalCalc(30, 2.5, 60);
 // //let interval = intervalCalc(10, 2.5, 60);
 // //let interval = intervalCalc(20, 10, 4 * 60);
 
-// console.log(interval);
-// console.log("");
+console.log(interval);
+console.log("X");
 console.log(calculateNextRunTime(startDate, interval, atDate));
+
+
+console.log(new moment("2017-06-01 08:23:34.234").diff(new moment("2017-06-01 07:00"), 'milliseconds') / 1000 / 60);
--- simple_test.js
+++ simple_test.js
@@ -4,13 +4,14 @@ const moment = require('moment');
 
 // https://wiki.onion.io/Tutorials/Expansions/Using-the-OLED-Expansion
 
-const time = new Date();
+
 // DATA STRUCTURES
 const testPump = {
 		channel : 1,
-		estimatedRate_ml_min : 5,
-		startTime : new moment("2017-05-31 00:00"),
-		duration_min : 120,
+        label : "Testing",
+		estimatedRate_ml_min : 10,
+		startTime : new moment("2017-05-31 00:06"),
+		duration_min : 30,
 		maxQuanityPerDay_ml : 100,
 		enabled : true,
 
@@ -19,18 +20,17 @@ const testPump = {
         pumpState : {
             state : "Normal",
             isRunning : false,
-            lastStarted : new moment("2017-05-29 08:00"),
-            stopAt : undefined,
-            stateForDay : new moment(),            
+            lastStarted : undefined,
+            lastStopped : undefined,
             estimatedVolumeToday_ml : 0,
-            estimatedVolumeCalculatedAt : new moment()
+            estimatedVolumeCheckpoint : undefined
         }
 	};
 
 // CONSTANTS
+const loopTimeoutInMs = 500; // Loop every 30 seconds
+const maxQuantityOverrun_ml = 5;
 let keepAlive = true;
-let loopTimeoutInMs = 500; // Loop every 30 seconds
-let runEvery_Minutes = 1;
 let tick = new moment();
 
 // CONDITIONAL OBJECTS
@@ -94,122 +94,123 @@ let intervalCalc = function(targetVolume_ml, rate_ml_min, totalTimespan_min) {
 }
 
 // uses interval and startDate to calculate the next run time.
-// atDate is for debugging and should be removed in production
-// Depends on startDate being equal to the current date
-let calculateNextRunTime = function(startDate, interval) {
-    let now = new moment();
+// relative to forDate (or NOW if omitted)
+//
+// Depends on startDate being equal to the current date!!
+// a return value of undefined indicates the current time is past the full run interval. 
+// The date part of startDate should be incremented to today's date to get the next run time.
+let calculateNextRunTime = function(startDate, interval, atDate) {
+    let now = atDate || new moment();
     let segmentNumber = 0;
     let currentMs = now - startDate;
     let currentRunTime = undefined;
 
     while(segmentNumber <= interval.divisor) {
 
-        var goTime = startDate.valueOf() + ((segmentNumber * interval.segmentTime_min) * 60 * 1000)
-        if(goTime >= now)
+        //startDate.clone().add(segmentNumber * interval.segmentTime_min, 'm'); //
+        var goTime = startDate.clone().add(segmentNumber * interval.segmentTime_min, 'm'); //startDate.valueOf() + ((segmentNumber * interval.segmentTime_min) * 60 * 1000)
+        if(goTime > now)
             return {
-                "thisRun" : currentRunTime,
-                "nextRun" : new moment(goTime),
-                "segment" : segmentNumber,
-                "now" : now
+                "now" : now,
+                "thisRun" : (currentRunTime || goTime), //if now is before startDate thisRun and nextRun are equivalent, thisStop will accurately reflect the initial segment stop.
+                "thisStop" : (currentRunTime || goTime).clone().add(interval.upTime_min, 'm'),
+                "nextRun" : goTime,
+                "segment" : segmentNumber
             };
         
-        currentRunTime = new moment(goTime); // track the most recent run time to be set as 'thisRun' on the next go around if the time checks.
+        currentRunTime = goTime; // track the most recent run time to be set as 'thisRun' on the next go around if the time checks.
         segmentNumber++;
     }
 
-    return undefined;
+
+    return undefined; 
 }
 
 // CORE DOSER LOGIC
 
-let getVolumeSinceLastCheck = function(pump) {
-    const isToday = (pump.pumpState.StateForDay === new moment());
-    if(pump.isRunning && isToday) {
-        var minutesSinceLastCalc 
-            = (new moment() - pump.pumpState.timeSinceLastCalc).valueOf()
-              / (1000 / 60);
-
-        return minutesSinceLastCalc * pump.estimatedRate_ml_min;
-    }
-    else {
-        return 0;
-    }
-}
-
 let updateVolumeEstimate = function(pump) {
-    pump.pumpState.estimatedVolumeCalculatedAt = new moment();
-    pump.pumpState.estimatedVolumeToday_ml += getVolumeSinceLastCheck(pump);   
+    if(pump.pumpState.isRunning) {
+        let now = new moment();
+
+        let minutesSinceLastCalc = now.diff(pump.pumpState.estimatedVolumeCheckpoint, 'milliseconds') / 1000 / 60;
+
+        doser.log(now.format() + " " + pump.pumpState.estimatedVolumeCheckpoint.format() + " Rate: " + pump.estimatedRate_ml_min + " Rate: " + minutesSinceLastCalc );
+
+        let newVolume = (minutesSinceLastCalc * pump.estimatedRate_ml_min);
+
+        pump.pumpState.estimatedVolumeCheckpoint = now;
+        pump.pumpState.estimatedVolumeToday_ml += newVolume;
+
+        doser.log('New volume: ' + newVolume + ' (ml)');    
+    }
+
+    doser.log('Estimated volume: ' + pump.pumpState.estimatedVolumeToday_ml + ' (ml)');
 }
 
-let startPumpRunning = function(pump, interval) {
+let startPumpRunning = function(pump, runTime) {
+    let now = new moment();
     // START THE PUMP
     pump.pumpState.isRunning = true;
-    pump.pumpState.lastStarted = new moment();
-    pump.pumpState.stopAt = pump.pumpState.lastStarted.clone().add(interval.upTime_min, 'm');
+    pump.pumpState.lastStarted = now;
+    pump.pumpState.estimatedVolumeCheckpoint = now;
 
-    doser.log("Pump STARTED! " + pump.pumpState.lastStarted.format() + " stop at " + pump.pumpState.stopAt.format());
-
-    // let shutdown = setTimeout(function() {
-    //     shutPumpDown(pump, interval);
-    // }, interval.upTime_min * 60 * 1000);
-
-    //setTimeout(shutPumpDown, interval.upTime_min * 60 * 1000, pump, interval);   
+    doser.log("Pump STARTED! " + pump.pumpState.lastStarted.format() + " stop at " + runTime.thisStop.format());
 }
 
 let shutPumpDown = function(pump) {
+    updateVolumeEstimate(pump);
     pump.pumpState.isRunning = false;
+
+    pump.pumpState.lastStarted = undefined;
     doser.log("Pump STOPPED! " + new moment().format());
 }
 
 let processPump = function(pump) {
     const now = new moment();
-    const tock = tick.clone().add(10, 's');
+    const tock = tick.clone().add(15, 's');
 
-    const isToday = (pump.pumpState.StateForDay === new moment());
-    const overMaxVolume = pump.pumpState.estimatedVolumeToday_ml > pump.maxQuanityPerDay_ml;
+    const overMaxVolume = pump.pumpState.estimatedVolumeToday_ml > (pump.maxQuanityPerDay_ml + maxQuantityOverrun_ml);
 
     if(overMaxVolume) {
         doser.setPower(pump, false); // OFF!!
         return;
     }    
 
+    // Calculate the interval and run time.
+    const interval = intervalCalc(pump.maxQuanityPerDay_ml, pump.estimatedRate_ml_min, pump.duration_min);
+    const runTime = calculateNextRunTime(pump.startTime, interval);
+
+    //TODO: if runtime is undefined, increment startDate and save to disk
+
+    if(tock < now) {
+        tick = now;
+    }
+
     // Pump is running, estimate new volume
     if (pump.pumpState.isRunning) {
-        //updateVolumeEstimate(pump);
-
         if(tock < now) {
-            doser.log(now.format() + "Running: " + pump.startTime.format() + " to " + pump.pumpState.stopAt.format());
-            tick = now;
-        }        
+            updateVolumeEstimate(pump);
+        }
 
-        if(pump.pumpState.stopAt >= now) {
+        if(now > runTime.thisStop) {
             shutPumpDown(pump);
         }        
     }
     // Pump is NOT running, Check for startup state.
     else  {
-        const interval = intervalCalc(pump.maxQuanityPerDay_ml, pump.estimatedRate_ml_min, pump.duration_min);
-        const runTime = calculateNextRunTime(pump.startTime, interval);
-
-        if(tock < now) {
-            doser.log("-||- using start time " + pump.startTime.format())        
-            doser.log(interval);
-            doser.log(runTime);
-            doser.log("____");
-
-            tick = now;
-        }
-
         if(runTime 
-            && runTime.thisRun < new moment() 
-            && pump.pumpState.lastStarted < runTime.thisRun 
+            && runTime.thisRun < now
+            && now < runTime.thisStop   // don't start prematurely
+            //&& pump.pumpState.lastStarted < runTime.thisRun 
             && !pump.pumpState.isRunning) {
 
             doser.log("GO!");
-            startPumpRunning(pump, interval);
+            doser.log(interval);
+            doser.log(runTime);
+            startPumpRunning(pump, runTime);
         }
         else {
-            doser.log("Don't run yet.");
+            //doser.log("Don't run yet.");
         }
     }
 }
@@ -228,7 +229,24 @@ let coreLoop = function() {
 }
 
 
+pumps.forEach(function(pump) {
+    let now = new moment();
+    pump.startTime.year(now.year());
+    pump.startTime.month(now.month());
+    pump.startTime.date(now.date());
+    
+    doser.log("Initialize pump Channel " + pump.channel + " " + pump.label );
+    doser.log("Start time: " + pump.startTime.format() );
+
+    const interval = intervalCalc(pump.maxQuanityPerDay_ml, pump.estimatedRate_ml_min, pump.duration_min);
+    const runTime = calculateNextRunTime(pump.startTime, interval);
+
+    doser.log(interval);
+    doser.log(runTime);
+}, this);
+
 // Entry point
-//coreLoop();
 coreLoop();
+//coreLoop();
 //let loopInterval = setInterval(coreLoop, loopTimeoutInMs);
+
